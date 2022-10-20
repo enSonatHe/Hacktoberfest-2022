49987096c9e802c2485557cf673ba421f24f9882
Dharmendra Sah <40175152+Dharam-shah@users.noreply.github.com>
2022-10-20 17:27:09 +0545

    testcase

---
+++ testcase.py
@@ -0,0 +1,13 @@
+import unittest
+
+def add_fish_to_aquarium(fish_list):
+    if len(fish_list) > 10:
+        raise ValueError("A maximum of 10 fish can be added to the aquarium")
+    return {"tank_a": fish_list}
+
+
+class TestAddFishToAquarium(unittest.TestCase):
+    def test_add_fish_to_aquarium_success(self):
+        actual = add_fish_to_aquarium(fish_list=["shark", "tuna"])
+        expected = {"tank_a": ["shark", "tuna"]}
+        self.assertEqual(actual, expected)
